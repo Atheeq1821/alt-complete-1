3275ca0ddbb1265c7143805b1798c67df856e05c
Syed Atheequr Rahaman <syed21ad067@rmkcet.ac.in>
2023-08-01 11:26:32 +0530

    Add files via upload

---
+++ about Us/about.js
@@ -0,0 +1,9 @@
+function copyClipboard()
+{
+    let phone =document.getElementById("phone")
+    phone.textContent="Copied"
+    navigator.clipboard.writeText("91762 66062")
+
+        
+           
+}
